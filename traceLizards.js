680dc8b8ae1dd164c4b08b5cb4761790cdcac7e8
AlienWare51 <AlienWare.51@aol.com>
2025-02-03 15:51:01 -0800

    Update traceLizards.js

--- traceLizards.js
+++ traceLizards.js
@@ -1,8 +1,8 @@
 function onOpenCvReady() {
     const images = [
-        'lizard1.jpg',  // Replace with actual image paths
-        'lizard2.jpg',  // Replace with actual image paths
-        'lizard3.jpg'   // Replace with actual image paths
+        'https://th.bing.com/th/id/R.1f47a179c2e05a05333a7f69aaf6957a?rik=QI5jx2AEcOgnyg&riu=http%3a%2f%2fwww.chameleonwebservices.co.uk%2fwp-content%2fuploads%2f2015%2f05%2fchameleon-image-7.jpg&ehk=tJlhQBhZfR%2bzIMxyvinl%2fsmX1eb16loPg72oZ%2bo8Zdk%3d&risl=&pid=ImgRaw&r=0',  // Replace with actual image paths
+        'https://www.thesprucepets.com/thmb/-QTn07Dcbk51mlkK9iaGyvYBOgs=/2000x1210/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-969103888-5c44a97fc9e77c0001901f03.jpg',  // Replace with actual image paths
+        'https://th.bing.com/th/id/OIP.SA01bBacA1Jy0zhYtsU_7wHaEM?rs=1&pid=ImgDetMain'   // Replace with actual image paths
     ];
 
     const canvasIds = ['canvas1', 'canvas2', 'canvas3'];
