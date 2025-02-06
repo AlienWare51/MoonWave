f98161e5055488c4dd1ce691431a90e46bfc6b26
AlienWare51 <AlienWare.51@aol.com>
2025-02-05 17:16:10 -0800

    Add files via upload

---
+++ ink.js
@@ -0,0 +1,100 @@
+一	yī	one; a, an; alone	一 1.0	1	1	0001	2
+乙	yǐ	second; 2nd heavenly stem	乙 5.0	1	5	0002	1872
+二	èr	two; twice	二 7.0	2	1	0003	157
+十	shí	ten, tenth; complete; perfect	十 24.0	2	1	0004	112
+丁	dīng	male adult; robust, vigorous; 4th heavenly stem	一 1.1	2	6	0005	1168
+厂	chǎng	factory, workshop; radical 27	厂 27.0	2	5	0006	963
+七	qī	seven	一 1.1	2	1	0007	530
+卜	bo	fortune telling; prophesy	卜 25.0	2		0008	1979
+八	bā	eight; all around, all sides	八 12.0	2	1	0009	451
+人	rén	man; people; mankind; someone else	人 9.0	2	1	0010	7
+入	rù	enter, come in(to), join	入 11.0	2	4	0011	210
+儿	ér	son, child; KangXi radical 10	儿 10.0	2	1	0012	192
+匕	bǐ	spoon, ladle; knife, dirk	匕 21.0	2		0013	3252
+几	jǐ	small table	几 16.0	2	1	0014	211
+九	jiǔ	nine	乙 5.1	2	1	0015	445
+刁	diāo	tricky, sly, crafty, cunning	刀 18.0	2		0016	3635
+了	le	to finish; particle of completed action	亅 6.1	2	1	0017	5
+刀	dāo	knife; old coin; measure	刀 18.0	2	4	0018	1067
+力	lì	power, capability, influence	力 19.0	2	3	0019	106
+乃	nǎi	then; really, indeed; as it turned out, after all; namely	丿 4.1	2		0020	1165
+又	yòu	and, also, again, in addition	又 29.0	2	3	0021	126
+三	sān	three	一 1.2	3	1	0022	125
+干	gàn	oppose, offend; invade; dried	干 51.0	3	3	0023	353
+于	yú	in, on, at; go to; surname	二 7.1	3	3	0024	40
+亏	kuī	lose, fail; damage; deficient	二 7.1	3	5	0025	1738
+工	gōng	labor, work; worker, laborer	工 48.0	3	1	0026	118
+土	tǔ	soil, earth; items made of earth	土 32.0	3	5	0027	515
+士	shì	scholar, gentleman; soldier	士 33.0	3	4	0028	372
+才	cái	talent, ability; just, only	手 64.0	3	3	0029	235
+下	xià	under, underneath, below; down; inferior; bring down	一 1.2	3	1	0030	42
+寸	cùn	inch; small, tiny; KangXi radical 41	寸 41.0	3	6	0031	1904
+大	dà	big, great, vast, large, high	大 37.0	3	1	0032	17
+丈	zhàng	unit of length equal 3.3 meters; gentleman, man, husband	一 1.2	3	2	0033	1242
+与	yǔ	and; with; to; for; give, grant	一 1.3	3	4	0034	108
+万	wàn	ten thousand; innumerable	一 1.2	3	3	0035	322
+上	shàng	top; superior, highest; go up, send up	一 1.2	3	1	0036	16
+小	xiǎo	small, tiny, insignificant	小 42.0	3	1	0037	83
+口	kǒu	mouth; open end; entrance, gate	口 30.0	3	3	0038	212
+山	shān	mountain, hill, peak	山 46.0	3	3	0039	259
+巾	jīn	kerchief; towel; turban; KangXi radical number 50	巾 50.0	3	4	0040	2281
+千	qiān	thousand; many, numerous; very; (Cant.) a cheater, swindler	十 24.1	3	2	0041	599
+乞	qǐ	beg; request	乙 5.2	3	6	0042	2429
+川	chuān	stream, river; flow; boil	巛 47.0	3	6	0043	1109
+亿	yì	hundred million; many	人 9.1	3	5	0044	1057
+个	gè	numerary adjunct, piece; single	丨 2.2	3	1	0045	12
+夕	xī	evening, night, dusk; slanted	夕 36.0	3	5	0046	2003
+久	jiǔ	long time (ago); time passage, grow late	丿 4.2	3	3	0047	637
+么	me	interrogative particle; repetition of a tune small; tender	丿 4.2	3	1	0048	63
+勺	sháo	spoon, ladle; unit of volume	勹 20.1	3	4	0049	3275
+凡	fán	all, any, every; ordinary, common	几 16.1	3	6	0050	1013
+丸	wán	small round object; pellet, pill	丶 3.2	3	6	0051	2462
+及	jí	extend; reach; come up to; and	又 29.2	3	4	0052	198
+广	guǎng	wide, extensive, broad; rad. 53	广 53.0	3	4	0053	468
+亡	wáng	death, destroyed; lose, perish	亠 8.1	3	6	0054	957
+门	mén	gate, door, entrance, opening	門 169.0	3	2	0055	185
+丫	yā	forked; bifurcation	丨 2.2	3		0056	2682
+义	yì	right conduct, righteousness	丿 4.2	3	5	0057	208
+之	zhī	marks preceding phrase as modifier of following phrase; it, him her, them; go to	丿 4.3	3	4	0058	44
+尸	shī	corpse; to impersonate the dead; to preside; KangXi radical 44	尸 44.0	3	6	0059	1517
+己	jǐ	self, oneself; personal, private; 6th heavenly stem	己 49.0	3	3	0060	162
+已	yǐ	already; finished; stop	己 49.0	3	2	0061	117
+巳	sì	the hours from 9 to 11; 6th terrestrial branch	己 49.0	3		0062	3381
+弓	gōng	bow; curved, arched; KangXi radical number 57	弓 57.0	3		0063	2229
+子	zi	offspring, child; fruit, seed of; 1st terrestrial branch	子 39.0	3	1	0064	37
+卫	wèi	guard, protect, defend	乙 5.2	3	4	0065	669
+也	yě	also; classical final particle of strong affirmation or identity	乙 5.2	3	2	0066	31
+女	nǚ	woman, girl; feminine; rad. 38	女 38.0	3	1	0067	224
+刃	rèn	edged tool, cutlery, knife edge	刀 18.1	3		0068	2700
+飞	fēi	fly; go quickly; dart; high	乙 5.2	3	1	0069	347
+习	xí	practice; flapping wings	冫 15.1	3	1	0070	676
+叉	chā	crotch; fork, prong	又 29.1	3	5	0071	2231
+马	mǎ	horse; surname; KangXi radical 187	馬 187.0	3	3	0072	276
+乡	xiāng	country; rural; village	幺 52.0	3	5	0073	922
+丰	fēng	abundant, lush, bountiful, plenty	丨 2.3	4	4	0074	1189
+王	wáng	king, ruler; royal; surname	玉 96.0	4	5	0075	299
+开	kāi	open; initiate, begin, start	廾 55.1	4	1	0076	94
+井	jǐng	well, mine shaft, pit	二 7.2	4	6	0077	1431
+天	tiān	sky, heaven; god, celestial	大 37.1	4	1	0078	78
+夫	fū	man, male adult, husband; those	大 37.1	4	2	0079	377
+元	yuán	first; dollar; origin; head	儿 10.2	4	3	0080	370
+无	wú	negative, no, not; KangXi radical 71	无 71.0	4	4	0081	105
+云	yún	say, speak; clouds	二 7.2	4	4	0082	692
+专	zhuān	monopolize, take sole possession	一 1.3	4	4	0083	485
+丐	gài	beggar; beg; give	一 1.3	4	6	0084	2606
+扎	zhā	pull up; pierce; struggle free	手 64.1	4	6	0085	1411
+艺	yì	art; talent, ability; craft	艸 140.1	4	4	0086	786
+木	mù	tree; wood, lumber; wooden	木 75.0	4	5	0087	694
+五	wǔ	five; surname	二 7.2	4	1	0088	279
+支	zhī	disperse, pay; support; branch	支 65.0	4	4	0089	437
+厅	tīng	hall, central room	厂 27.2	4	4	0090	1217
+不	bù	no, not; un-; negative prefix	一 1.3	4	1	0091	4
+犬	quǎn	dog; radical number 94	犬 94.0	4	6	0092	2649
+太	tài	very, too, much; big; extreme	大 37.1	4	1	0093	240
+区	qū	area, district, region, ward	匸 23.2	4	4	0094	265
+历	lì	history; calendar	厂 27.2	4	3	0095	480
+歹	dǎi	bad, vicious, depraved, wicked	歹 78.0	4	6	0096	2905
+友	yǒu	friend, companion; fraternity	又 29.2	4	1	0097	594
+尤	yóu	especially, particularly	尢 43.1	4	4	0098	1099
+匹	pǐ	bolt of cloth; counter for horses	匸 23.2	4	5	0099	1908
+车	chē	cart, vehicle; carry in cart	車 159.0	4	1	0100	361
